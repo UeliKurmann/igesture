2172664dc4fdf817e01e8a82cbcb6fa12b9282cd
bsigner <bsigner@f93bbda9-e428-0410-8f24-ec21284828af>
2007-02-24 21:09:42 +0000

    - Cleanup

--- igesture-framework/src/main/java/org/ximtec/igesture/Recogniser.java
+++ igesture-framework/src/main/java/org/ximtec/igesture/Recogniser.java
@@ -3,7 +3,7 @@
  *
  * Author       :   Ueli Kurmann, kurmannu@ethz.ch
  *
- * Purpose      :	Front-End of the iGesture Framework 
+ * Purpose      :	Front-end of the iGesture framework 
  *
  * -----------------------------------------------------------------------
  *
@@ -11,7 +11,8 @@
  *
  * Date             Who         Reason
  *
- * 26.12.2006       ukurmann    Initial Release
+ * Dec 26, 2006     ukurmann    Initial Release
+ * Feb 24, 2007     bsigner     Cleanup
  *
  * -----------------------------------------------------------------------
  *
@@ -40,15 +41,20 @@ import org.ximtec.igesture.event.EventManager;
 import org.ximtec.igesture.util.XMLTools;
 
 
+/**
+ * Front-end of the iGesture framework.
+ * @version 1.0, Dec 2006
+ * @author Ueli Kurmann, kurmannu@ethz.ch
+ * @author Beat Signer, signer@inf.ethz.ch
+ */
 public class Recogniser {
 
    private List<Algorithm> algorithms;
 
 
    /**
-    * Creates a new recogniser
-    * 
-    * @param config the configuration object
+    * Creates a new recogniser.
+    * @param config the configuration object.
     */
    public Recogniser(Configuration config) throws AlgorithmException {
       algorithms = AlgorithmFactory.createAlgorithms(config);
@@ -56,24 +62,22 @@ public class Recogniser {
 
 
    /**
-    * Create a new recogniser
-    * 
-    * @param config the configuration object
-    * @param eventManager the event manager
+    * Creates a new recogniser.
+    * @param config the configuration object.
+    * @param eventManager the event manager.
     */
    public Recogniser(Configuration config, EventManager eventManager)
          throws AlgorithmException {
-      final Configuration clone = (Configuration) config.clone();
+      final Configuration clone = (Configuration)config.clone();
       clone.setEventManager(eventManager);
       algorithms = AlgorithmFactory.createAlgorithms(clone);
    }
 
 
    /**
-    * Creates a new recogniser
-    * 
-    * @param config
-    * @param gestureSet
+    * Creates a new recogniser.
+    * @param config the configuration object.
+    * @param gestureSet the gesture set to be used.
     */
    public Recogniser(Configuration config, GestureSet gestureSet)
          throws AlgorithmException {
@@ -83,9 +87,8 @@ public class Recogniser {
 
 
    /**
-    * Creates a new recogniser
-    * 
-    * @param configFile the file containing the XML configuration file
+    * Creates a new recogniser.
+    * @param configFile the file containing the XML configuration.
     */
    public Recogniser(File configFile) throws AlgorithmException {
       this(XMLTools.importConfiguration(configFile));
@@ -93,10 +96,9 @@ public class Recogniser {
 
 
    /**
-    * Creates a new recogniser
-    * 
-    * @param configFile the configuration XML file
-    * @param setFile the gesture set XML file
+    * Creates a new recogniser.
+    * @param configFile the file containing the XML configuration.
+    * @param setFile the file containing the gesture set in XML format.
     * @param eventManager the event manager
     */
    public Recogniser(File configFile, File setFile, EventManager eventManager)
@@ -109,34 +111,42 @@ public class Recogniser {
 
 
    /**
-    * Recognies a gesture. The method uses all algorithms and combines the result
-    * set of them.
-    * 
-    * @param note the note to recognise
-    * @return the ResultSet containing the recognised gesture classes
+    * Recognies a gesture. The method uses all algorithms and retunrs a
+    * comination of the results returned by the different algorithms if
+    * 'recogniseAll' is set to true or the result of the first algorithm that
+    * recognised the given note if 'recogniseAll' is set to false.
+    * @param note the note to be recognised.
+    * @param recogniseAll true if the combination of all algorithms has to be
+    *           returned, false if the result of the first algorithm that
+    *           recognises the result has to be returned only.
+    * @return the result set containing the recognised gesture classes.
     */
    public ResultSet recognise(Note note, boolean recogniseAll) {
       final ResultSet result = new ResultSet();
+
       for (final Algorithm algorithm : algorithms) {
+
          for (final Result r : algorithm.recognise(note).getResults()) {
             result.add(r);
          }
+
          if (!result.isEmpty() && !recogniseAll) {
             break;
          }
+
       }
       return result;
-   }
+   } // recognise
 
 
    /**
-    * Recognies a gesture. The method uses the algorithms registred and stops as
+    * Recognies a gesture. The method uses all registered algorithms and stops as
     * soon as the first algorithm returns a result.
-    * 
-    * @param note the note to recognise
-    * @return the ResultSet containing the recognised gesture classes
+    * @param note the note to be recognised.
+    * @return the result set containing the recognised gesture classes.
     */
    public ResultSet recognise(Note note) {
       return recognise(note, false);
-   }
+   } // recognise
+
 }
