74167790672ab5877dbd5bbb6d82e47b5280ecd8
bsigner <bsigner@f93bbda9-e428-0410-8f24-ec21284828af>
2007-03-22 11:33:22 +0000

    - Cleanup

--- trunk/igesture-framework/src/main/java/org/ximtec/igesture/batch/BatchTools.java
+++ trunk/igesture-framework/src/main/java/org/ximtec/igesture/batch/BatchTools.java
@@ -3,7 +3,7 @@
  *
  * Author       :   Ueli Kurmann, kurmannu@ethz.ch
  *
- * Purpose      : 	Helper metods used by the batch tools
+ * Purpose      : 	Some helper methods.
  *
  * -----------------------------------------------------------------------
  *
@@ -11,7 +11,8 @@
  *
  * Date             Who         Reason
  *
- * 26.12.2006       ukurmann    Initial Release
+ * Dec 26, 2006     ukurmann    Initial Release
+ * Mar 22, 2007     bsigner     Cleanup
  *
  * -----------------------------------------------------------------------
  *
@@ -32,76 +33,80 @@ import java.util.StringTokenizer;
 
 
 /**
- * Toll methods used for batch processing
- * @author Ueli Kurmann
- * @version 1.0
+ * Some helper methods.
+ * 
+ * @version 1.0 Dec 2006
+ * @author Ueli Kurmann, kurmannu@ethz.ch
+ * @author Beat Signer, signer@inf.ethz.ch
  */
 public class BatchTools {
 
    /**
-    * Returns a collection of String Elements. The input is a String List with a
+    * Returns a collection of string elements. The input is a string list with a
     * given delimiter.
     * 
-    * @param list
-    * @param delimiter
-    * @return
+    * @param list the string list.
+    * @param delimiter the delimiter used within the strng list.
+    * @return the parsed list of string elements.
     */
+   // FIXME: that is general functionality that could be added to sigtec. Would
+   // that be ok for you Ueli if I move that in sigtec?
    public static List<String> parseList(String list, String delimiter) {
       final StringTokenizer tokenizer = new StringTokenizer(list, delimiter);
       final List<String> elements = new ArrayList<String>();
+
       while (tokenizer.hasMoreTokens()) {
          elements.add(tokenizer.nextToken());
       }
+
       return elements;
-   }
+   } // parseList
 
 
    /**
-    * Filters a Set of Sets. The condition are the number of elements in the set.
+    * Filters a set of sets. The condition are the number of elements in the set.
     * 
-    * @param powerSet the set to filter
-    * @param min the minimal number of elements in the set
-    * @param max the maximal number of elements in the set
-    * @return the filtered set
+    * @param powerSet the set to be filtered.
+    * @param min the minimal number of elements in the set.
+    * @param max the maximal number of elements in the set.
+    * @return the filtered set.
     */
    public static HashSet<HashSet<String>> filterSet(
          HashSet<HashSet<String>> powerSet, int min, int max) {
       final HashSet<HashSet<String>> result = new HashSet<HashSet<String>>();
+
       for (final HashSet<String> set : powerSet) {
+
          if (set.size() >= min && set.size() <= max) {
             result.add(set);
          }
+
       }
       return result;
-   }
+   } // filterSet
 
 
    /**
     * Creates the power set of a given set.
     * 
-    * @param set the set to create the power set
-    * @return the power set of the set
+    * @param set the set a power set has to be created from.
+    * @return the power set.
     */
    public static HashSet<HashSet<String>> getPowerSet(HashSet<String> set) {
       final HashSet<HashSet<String>> powerSet = new HashSet<HashSet<String>>();
       final HashSet<String> currSet = new HashSet<String>();
       return createPowerSet(powerSet, currSet, 0, set);
-   }
+   } // getPowerSet
 
 
    /**
-    * Creates the power set recursively
-    * 
-    * @param powerSet
-    * @param currentSet
-    * @param index
-    * @param set
-    * @return
+    * Creates the power set (recursively).
     */
    @SuppressWarnings("unchecked")
    private static HashSet<HashSet<String>> createPowerSet(
          HashSet<HashSet<String>> powerSet, HashSet<String> currentSet,
          int index, HashSet<String> set) {
+
       if (index == set.size()) {
          final HashSet<HashSet<String>> ps = (HashSet<HashSet<String>>)powerSet
                .clone();
@@ -116,6 +121,6 @@ public class BatchTools {
          return powerSet;
       }
 
-   }
+   } // createPowerSet
 
 }
