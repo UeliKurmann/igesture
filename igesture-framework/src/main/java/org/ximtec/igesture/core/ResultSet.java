2b835b49158e7598b297eb715376e04364017ecc
bsigner <bsigner@f93bbda9-e428-0410-8f24-ec21284828af>
2007-04-30 13:42:51 +0000

    - Cleanup

--- igesture-framework/src/main/java/org/ximtec/igesture/core/ResultSet.java
+++ igesture-framework/src/main/java/org/ximtec/igesture/core/ResultSet.java
@@ -23,6 +23,7 @@
  * 
  */
 
+
 package org.ximtec.igesture.core;
 
 import java.util.ArrayList;
@@ -36,6 +37,7 @@ import java.util.TreeSet;
 import org.sigtec.ink.Note;
 import org.sigtec.util.Constant;
 
+
 /**
  * Holds a set of result objects.
  * 
@@ -45,158 +47,171 @@ import org.sigtec.util.Constant;
  */
 public class ResultSet {
 
-	private SortedSet<Result> results;
+   private static final String RESULT_SET = "ResultSet";
 
-	private int maxSize;
+   private SortedSet<Result> results;
 
-	private Note note;
+   private int maxSize;
 
-	/**
-	 * Constructs a new result set.
-	 */
-	public ResultSet() {
-		this(Integer.MAX_VALUE);
-	}
+   private Note note;
 
-	/**
-	 * Constructs a new result set with a specific maximal size.
-	 * 
-	 * @param maxSize
-	 *            the maximal size of the result set.
-	 */
-	public ResultSet(int maxSize) {
-		results = Collections.synchronizedSortedSet(new TreeSet<Result>(
-				new Comparator<Result>() {
-					public int compare(Result result1, Result result2) {
-						return Double.compare(result2.getAccuracy(), result1
-								.getAccuracy());
-					}
-				}));
-		this.maxSize = maxSize;
-	}
 
-	/**
-	 * Adds a result object to the result set. The result set is always ordered
-	 * by the accuracy value of the result objects.
-	 * 
-	 * @param result
-	 *            the result object to be added.
-	 */
-	public synchronized void addResult(Result result) {
-		results.add(result);
-	} // addResult
+   /**
+    * Constructs a new result set.
+    */
+   public ResultSet() {
+      this(Integer.MAX_VALUE);
+   }
 
-	/**
-	 * Adds a collection of results
-	 * 
-	 * @param results
-	 */
-	public void addResults(Collection<Result> results) {
-		for (Result result : results) {
-			addResult(result);
-		}
-	}// addResults
 
-	/**
-	 * Returns a list containing all result objects.
-	 * 
-	 * @return the list of result objects.
-	 */
-	public List<Result> getResults() {
-		return new ArrayList<Result>(results).subList(0, Math.min(maxSize,
-				results.size()));
-	} // getResults
+   /**
+    * Constructs a new result set with a specific maximal size.
+    * 
+    * @param maxSize the maximal size of the result set.
+    */
+   public ResultSet(int maxSize) {
+      results = Collections.synchronizedSortedSet(new TreeSet<Result>(
+            new Comparator<Result>() {
 
-	/**
-	 * Returns the result for a given index.
-	 * 
-	 * @param index
-	 *            the index of the result object to be returned.
-	 * @return the result object for the given index position.
-	 */
-	public Result getResult(int index) {
-		return new ArrayList<Result>(results).get(index);
-	} // getResult
+               public int compare(Result result1, Result result2) {
+                  return Double.compare(result2.getAccuracy(), result1
+                        .getAccuracy());
+               }
+            }));
+      this.maxSize = maxSize;
+   }
 
-	/**
-	 * Returns the first result object which is also the result object with the
-	 * highest recognition accuracy.
-	 * 
-	 * @return the first result object or null if the result set does not
-	 *         contain any result obejcts.
-	 */
-	public Result getResult() {
-		return !results.isEmpty() ? getResult(0) : null;
-	} // getResult
 
-	/**
-	 * Returns true if the result set contains the given gesture class.
-	 * 
-	 * @param gestureClass
-	 *            the gesture class to be tested.
-	 * @return true if the result set contains the specified gesture class.
-	 */
-	public boolean contains(GestureClass gestureClass) {
-		for (final Result result : results) {
+   /**
+    * Adds a result object to the result set. The result set is always ordered by
+    * the accuracy value of the result objects.
+    * 
+    * @param result the result object to be added.
+    */
+   public synchronized void addResult(Result result) {
+      results.add(result);
+   } // addResult
 
-			if (result.getGestureClass() == gestureClass) {
-				return true;
-			}
 
-		}
-		return false;
-	} // contains
+   /**
+    * Adds a collection of results
+    * 
+    * @param results
+    */
+   public void addResults(Collection<Result> results) {
+      for (Result result : results) {
+         addResult(result);
+      }
+   }// addResults
 
-	/**
-	 * Returns the number of results.
-	 * 
-	 * @return the number of results in the result set.
-	 */
-	public int size() {
-		return results.size() > maxSize ? maxSize : results.size();
-	} // size
 
-	/**
-	 * Return true if the result set is empty.
-	 * 
-	 * @return true if the result set is empty.
-	 */
-	public boolean isEmpty() {
-		return results.isEmpty();
-	} // isEmpty
+   /**
+    * Returns a list containing all result objects.
+    * 
+    * @return the list of result objects.
+    */
+   public List<Result> getResults() {
+      return new ArrayList<Result>(results).subList(0, Math.min(maxSize, results
+            .size()));
+   } // getResults
 
-	/**
-	 * Sets the note containing the gesture.
-	 * 
-	 * @param note
-	 *            the note containing the gesture.
-	 */
-	public void setNote(Note note) {
-		this.note = note;
-	} // setNote
 
-	/**
-	 * Returns the note containing the gesture to be recognised.
-	 * 
-	 * @return the note containing the gesture to be recognised.
-	 */
-	public Note getNote() {
-		return note;
-	} // getNote
+   /**
+    * Returns the result for a given index.
+    * 
+    * @param index the index of the result object to be returned.
+    * @return the result object for the given index position.
+    */
+   public Result getResult(int index) {
+      return new ArrayList<Result>(results).get(index);
+   } // getResult
 
-	@Override
-	public String toString() {
-		final StringBuilder sb = new StringBuilder();
-		sb.append("[ResultSet");
 
-		for (final Result r : this.getResults()) {
-			sb.append(Constant.DOUBLE_BLANK + Constant.OPEN_ANGULAR_BRACKET
-					+ r.getGestureClassName() + Constant.COMMA_BLANK
-					+ r.getAccuracy() + Constant.CLOSE_ANGULAR_BRACKET);
-		}
+   /**
+    * Returns the first result object which is also the result object with the
+    * highest recognition accuracy.
+    * 
+    * @return the first result object or null if the result set does not contain
+    *         any result objects.
+    */
+   public Result getResult() {
+      return !results.isEmpty() ? getResult(0) : null;
+   } // getResult
 
-		sb.append(Constant.CLOSE_ANGULAR_BRACKET);
-		return sb.toString();
-	} // toString
+
+   /**
+    * Returns true if the result set contains the given gesture class.
+    * 
+    * @param gestureClass the gesture class to be tested.
+    * @return true if the result set contains the specified gesture class.
+    */
+   public boolean contains(GestureClass gestureClass) {
+      for (final Result result : results) {
+
+         if (result.getGestureClass() == gestureClass) {
+            return true;
+         }
+
+      }
+
+      return false;
+   } // contains
+
+
+   /**
+    * Returns the number of results.
+    * 
+    * @return the number of results in the result set.
+    */
+   public int size() {
+      return results.size() > maxSize ? maxSize : results.size();
+   } // size
+
+
+   /**
+    * Return true if the result set is empty.
+    * 
+    * @return true if the result set is empty.
+    */
+   public boolean isEmpty() {
+      return results.isEmpty();
+   } // isEmpty
+
+
+   /**
+    * Sets the note containing the gesture.
+    * 
+    * @param note the note containing the gesture.
+    */
+   public void setNote(Note note) {
+      this.note = note;
+   } // setNote
+
+
+   /**
+    * Returns the note containing the gesture to be recognised.
+    * 
+    * @return the note containing the gesture to be recognised.
+    */
+   public Note getNote() {
+      return note;
+   } // getNote
+
+
+   @Override
+   public String toString() {
+      final StringBuilder sb = new StringBuilder();
+      sb.append(Constant.OPEN_ANGULAR_BRACKET);
+      sb.append(RESULT_SET);
+
+      for (final Result r : this.getResults()) {
+         sb.append(Constant.DOUBLE_BLANK + Constant.OPEN_ANGULAR_BRACKET
+               + r.getGestureClassName() + Constant.COMMA_BLANK
+               + r.getAccuracy() + Constant.CLOSE_ANGULAR_BRACKET);
+      }
+
+      sb.append(Constant.CLOSE_ANGULAR_BRACKET);
+      return sb.toString();
+   } // toString
 
 }
