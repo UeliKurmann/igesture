af56cc91baa9734b879ef69e9fa3d930c067cfb0
bsigner <bsigner@f93bbda9-e428-0410-8f24-ec21284828af>
2007-05-01 13:38:32 +0000

    - Added new constants for different gesture sets

---
+++ trunk/igesture-framework/src/main/java/org/ximtec/igesture/gestureset/MSApplication.java
@@ -0,0 +1,123 @@
+/*
+ * @(#)MSApplication.java	1.0   May 1, 2007
+ *
+ * Author		:	Beat Signer, signer@inf.ethz.ch
+ *
+ * Purpose		:   Some constants for the Microsoft Application Gesture
+ *                  set.
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Revision Information:
+ *
+ * Date				Who			Reason
+ *
+ * Mar 1, 2007		bsigner		Initial Release
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright 1999-2007 ETH Zurich. All Rights Reserved.
+ *
+ * This software is the proprietary information of ETH Zurich.
+ * Use is subject to license terms.
+ * 
+ */
+
+
+package org.ximtec.igesture.gestureset;
+
+/**
+ * Some constants for the Microsoft Application Gesture set.
+ * @version 1.0 May 1, 2007
+ * @author Beat Signer, signer@inf.ethz.ch
+ */
+public class MSApplication {
+
+   public static final String GESTURE_SET = "gestureSets/ms_application_gestures.xml";
+   
+   public static final String SCRATCH_OUT = "Scratch-out";
+   
+   public static final String TRIANGLE = "Triangle";
+
+   public static final String SQUARE = "Square";
+   
+   public static final String STAR = "Star";
+   
+   public static final String CHECK = "Check";
+   
+   public static final String CURLICUE = "Curlicue";
+   
+   public static final String DOUBLE_CURLICUE = "Double-Curlicue";
+   
+   public static final String CIRCLE = "Circle";
+   
+   public static final String DOUBLE_CIRCLE = "Double-circle";
+   
+   public static final String LEFT_SEMICIRCLE = "Left-semicircle";
+   
+   public static final String RIGHT_SEMICIRCLE = "Right-semicircle";
+   
+   public static final String CARET = "Caret";
+   
+   public static final String INVERTED_CARET = "Inverted-caret";
+   
+   public static final String CHEVRON_LEFT = "Chevron-left";
+   
+   public static final String CHEVRON_RIGHT = "Chevron-right";
+   
+   public static final String ARROW_UP = "Arrow-up";
+   
+   public static final String ARROW_DOWN = "Arrow-down";
+   
+   public static final String ARROW_LEFT = "Arrow-left";
+   
+   public static final String ARROW_RIGHT = "Arrow-right";
+   
+   public static final String UP = "Up";
+   
+   public static final String DOWN = "Down";
+   
+   public static final String LEFT = "Left";
+   
+   public static final String RIGHT = "Right";
+   
+   public static final String UP_LEFT = "Up-left";
+   
+   public static final String UP_RIGHT = "Up-right";
+   
+   public static final String DOWN_LEFT = "Down-left";
+   
+   public static final String DOWN_RIGHT = "Down-right";
+   
+   public static final String LEFT_UP = "Left-up";
+   
+   public static final String LEFT_DOWN = "Left-down";
+   
+   public static final String RIGHT_UP = "Right-up";
+   
+   public static final String RIGHT_DOWN = "Right-down";
+   
+   public static final String UP_DOWN = "Up-down";
+   
+   public static final String DOWN_UP = "Down-up";
+   
+   public static final String LEFT_RIGHT = "Left-right";
+   
+   public static final String RIGHT_LEFT = "Right-Left";
+   
+   public static final String UP_LEFT_LONG = "Up-left-long";
+   
+   public static final String UP_RIGHT_LONG = "Up-right-long";
+   
+   public static final String DOWN_LEFT_LONG = "Down-left-long";
+   
+   public static final String DOWN_RIGHT_LONG = "Down-right-long";
+   
+   public static final String EXCLAMATION = "Exclamation";
+   
+   // FIXME: not yet in the gesture set?
+   // public static final String TAP = "Tap";
+   
+   // public static final String DOUBLE_TAP = "Double-tap";
+   
+}
---
+++ trunk/igesture-framework/src/main/java/org/ximtec/igesture/gestureset/MultiStroke.java
@@ -0,0 +1,67 @@
+/*
+ * @(#)MultiStroke.java	1.0   May 1, 2007
+ *
+ * Author		:	Beat Signer, signer@inf.ethz.ch
+ *
+ * Purpose		:   Some constants for the multi-stroke gesture set.
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Revision Information:
+ *
+ * Date				Who			Reason
+ *
+ * Mar 1, 2007		bsigner		Initial Release
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright 1999-2007 ETH Zurich. All Rights Reserved.
+ *
+ * This software is the proprietary information of ETH Zurich.
+ * Use is subject to license terms.
+ * 
+ */
+
+
+package org.ximtec.igesture.gestureset;
+
+/**
+ * Some constants for the multi-stroke gesture set.
+ * @version 1.0 May 1, 2007
+ * @author Beat Signer, signer@inf.ethz.ch
+ */
+public class MultiStroke {
+
+   public static final String GESTURE_SET = "gestureSets/multistroke_gestures.xml";
+
+   public static final String DOUBLE_LINE = "Double-Line";
+
+   public static final String X = "X";
+
+   public static final String ARROW_LEFT_RIGHT = "Arrow-Left-Right";
+   
+   public static final String ARROW_RIGHT_LEFT = "Arrow-Right-Left";
+   
+   public static final String DOLLAR = "Dollar";
+   
+   public static final String TIC_TAC_TOE = "TicTacToe";
+   
+   public static final String SMILEY = "Smiley";
+   
+   public static final String SEMI_CIRCLES = "Semi-Circles";
+   
+   public static final String ANGLE = "Angle";
+   
+   public static final String MEAN = "Mean";
+   
+   public static final String STAR = "Star";
+   
+   public static final String INTERFACE = "Interface";
+   
+   public static final String CLASS = "Class";
+   
+   public static final String DOWN = "Down";
+   
+   public static final String LEFT_RIGHT = "Left-Right";
+
+}
