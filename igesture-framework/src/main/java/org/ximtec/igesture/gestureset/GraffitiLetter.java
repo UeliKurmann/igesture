9fa1e9cd459a2ceb5fb2b0c559c0e7be15474240
bsigner <bsigner@f93bbda9-e428-0410-8f24-ec21284828af>
2007-05-01 13:38:32 +0000

    - Added new constants for different gesture sets

---
+++ igesture-framework/src/main/java/org/ximtec/igesture/gestureset/GraffitiLetter.java
@@ -0,0 +1,89 @@
+/*
+ * @(#)GraffitiLetter.java	1.0   May 1, 2007
+ *
+ * Author		:	Beat Signer, signer@inf.ethz.ch
+ *
+ * Purpose		:   Some constants for the Palm Graffiti letter set.
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Revision Information:
+ *
+ * Date				Who			Reason
+ *
+ * Mar 1, 2007		bsigner		Initial Release
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright 1999-2007 ETH Zurich. All Rights Reserved.
+ *
+ * This software is the proprietary information of ETH Zurich.
+ * Use is subject to license terms.
+ * 
+ */
+
+
+package org.ximtec.igesture.gestureset;
+
+/**
+ * Some constants for the Palm Graffiti letter set.
+ * @version 1.0 May 1, 2007
+ * @author Beat Signer, signer@inf.ethz.ch
+ */
+public class GraffitiLetter {
+
+   public static final String GESTURE_SET = "gestureSets/graffiti_letters.xml";
+
+   public static final String A = "A_Graffiti";
+
+   public static final String B = "B_Graffiti";
+
+   public static final String C = "C_Graffiti";
+
+   public static final String D = "D_Graffiti";
+
+   public static final String E = "E_Graffiti";
+
+   public static final String F = "F_Graffiti";
+
+   public static final String G = "G_Graffiti";
+
+   public static final String H = "H_Graffiti";
+
+   public static final String I = "I_Graffiti";
+
+   public static final String J = "J_Graffiti";
+
+   public static final String K = "K_Graffiti";
+
+   public static final String L = "L_Graffiti";
+
+   public static final String M = "M_Graffiti";
+
+   public static final String N = "N_Graffiti";
+
+   public static final String O = "O_Graffiti";
+
+   public static final String P = "P_Graffiti";
+
+   public static final String Q = "Q_Graffiti";
+
+   public static final String R = "R_Graffiti";
+
+   public static final String S = "S_Graffiti";
+
+   public static final String T = "T_Graffiti";
+
+   public static final String U = "U_Graffiti";
+
+   public static final String V = "V_Graffiti";
+
+   public static final String W = "W_Graffiti";
+
+   public static final String X = "X_Graffiti";
+
+   public static final String Y = "Y_Graffiti";
+
+   public static final String Z = "Z_Graffiti";
+
+}
---
+++ igesture-framework/src/main/java/org/ximtec/igesture/gestureset/GraffitiNumber.java
@@ -0,0 +1,57 @@
+/*
+ * @(#)GraffitiNumber.java	1.0   May 1, 2007
+ *
+ * Author		:	Beat Signer, signer@inf.ethz.ch
+ *
+ * Purpose		:   Some constants for the Palm Graffiti number set.
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Revision Information:
+ *
+ * Date				Who			Reason
+ *
+ * Mar 1, 2007		bsigner		Initial Release
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright 1999-2007 ETH Zurich. All Rights Reserved.
+ *
+ * This software is the proprietary information of ETH Zurich.
+ * Use is subject to license terms.
+ * 
+ */
+
+
+package org.ximtec.igesture.gestureset;
+
+/**
+ * Some constants for the Palm Graffiti number set.
+ * @version 1.0 May 1, 2007
+ * @author Beat Signer, signer@inf.ethz.ch
+ */
+public class GraffitiNumber {
+
+   public static final String GESTURE_SET = "gestureSets/graffiti_numbers.xml";
+
+   public static final String NUMBER_0 = "0_Graffiti";
+
+   public static final String NUMBER_1 = "1_Graffiti";
+
+   public static final String NUMBER_2 = "2_Graffiti";
+
+   public static final String NUMBER_3 = "3_Graffiti";
+
+   public static final String NUMBER_4 = "4_Graffiti";
+
+   public static final String NUMBER_5 = "5_Graffiti";
+
+   public static final String NUMBER_6 = "6_Graffiti";
+
+   public static final String NUMBER_7 = "7_Graffiti";
+
+   public static final String NUMBER_8 = "8_Graffiti";
+
+   public static final String NUMBER_9 = "9_Graffiti";
+
+}
