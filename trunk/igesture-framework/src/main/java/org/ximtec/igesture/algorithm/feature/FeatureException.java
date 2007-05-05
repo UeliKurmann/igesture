6a872e018da6055571ba44e84f3cb46760c990a2
kurmannu <kurmannu@f93bbda9-e428-0410-8f24-ec21284828af>
2007-05-05 11:08:23 +0000

    Add FeatureException class

---
+++ trunk/igesture-framework/src/main/java/org/ximtec/igesture/algorithm/feature/FeatureException.java
@@ -0,0 +1,38 @@
+/*
+ * @(#)FeatureException.java   1.0   May 5, 2007
+ *
+ * Author       :   Ueli Kurmann, kurmannu@ethz.ch
+ *
+ * Purpose      : 	Feature Exception
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Revision Information:
+ *
+ * Date             Who         Reason
+ *
+ * May 5, 2007      ukurmann    Initial Release
+ * 
+ * $Id$
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright 1999-2007 ETH Zurich. All Rights Reserved.
+ *
+ * This software is the proprietary information of ETH Zurich.
+ * Use is subject to license terms.
+ * 
+ */
+
+package org.ximtec.igesture.algorithm.feature;
+
+/**
+ * Feature Exception
+ * 
+ * @version 1.0 May 2007
+ * @author Ueli Kurmann, kurmannu@ethz.ch
+ * @author Beat Signer, signer@inf.ethz.ch
+ */
+public class FeatureException extends Exception {
+
+}
