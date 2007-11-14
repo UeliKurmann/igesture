9e4d109db208f76f7456069e412a977a5b4b6bec
crocimi <crocimi@f93bbda9-e428-0410-8f24-ec21284828af>
2007-11-14 15:49:46 +0000

    igesture-io initial release

---
+++ igesture-io/src/main/java/org/ximtec/igesture/io/wacom/Wintab32.java
@@ -0,0 +1,125 @@
+package org.ximtec.igesture.io.wacom;
+
+
+
+
+//import com.sun.jna.FromNativeContext;
+//import com.sun.jna.IntegerType;
+import org.ximtec.igesture.io.win32.W32API;
+
+import com.sun.jna.Native;
+
+import com.sun.jna.Structure;
+import com.sun.jna.win32.StdCallLibrary;
+
+
+/** Provides access to the w32 wintab32 library.
+ *  
+ * @author Michele Croci
+ */
+
+public interface Wintab32 extends W32API {
+
+		
+	
+
+	    public Wintab32 INSTANCE = (Wintab32)
+	    Native.loadLibrary("wintab32", Wintab32.class, DEFAULT_OPTIONS);
+	    
+	    static int HWND_BROADCAST = 0xffff;
+    	static int WTI_DEFCONTEXT = 3; //first parameter for WTInfo (not move cursor)
+    	static int WTI_DEFSYSCTX = 4; //first parameter for WTInfo (move cursor)
+    	static int WTI_DEVICES = 100; //status info about device
+
+	    HDC WTOpenW(HWND hwnd, LOGCONTEXTW s, boolean b);
+	    
+	    boolean WTClose(HDC hDC);
+	    
+	    int WTInfoW(int category, int index, LOGCONTEXTW s);
+	    
+	    int WTQueueSizeGet(HDC hdc);
+	    
+	    //The return value is the number of packets copied in the buffer
+	    int WTPacketsGet(HDC hdc, int max, PACKET p);
+	    
+
+	    HANDLE WTMgrPacketHookEx(HINSTANCE hmgr, int nType, String module, String hookproc); //not sure
+	    
+	    
+	    public static class LOGCONTEXTW extends Structure {
+	    	// see wintab32.dll specification
+	    	
+			//public String lcName;
+			public char[] lcName=new char[40];
+			public int	lcOptions;
+			public int	lcStatus;
+			public int	lcLocks;
+			public int	lcMsgBase;
+			public int	lcDevice;
+			public int	lcPktRate;
+			public int	lcPktData; 
+			public int	lcPktMode;
+			public int	lcMoveMask; 
+			public int	lcBtnDnMask;
+			public int	lcBtnUpMask;
+			public int	lcInOrgX;
+			public int	lcInOrgY;
+			public int	lcInOrgZ;
+			public int	lcInExtX;
+			public int	lcInExtY;
+			public int	lcInExtZ;
+			public int	lcOutOrgX;
+			public int	lcOutOrgY;
+			public int	lcOutOrgZ;
+			public int	lcOutExtX;
+			public int	lcOutExtY;
+			public int	lcOutExtZ;
+			public int	lcSensX;
+			public int	lcSensY;
+			public int	lcSensZ;
+			public boolean	lcSysMode;
+			public int		lcSysOrgX;
+			public int		lcSysOrgY;
+			public int		lcSysExtX;
+			public int		lcSysExtY;
+			public int	lcSysSensX;
+			public int	lcSysSensY;
+			
+	    }
+
+	    // for specification see wintab.h
+	    public static class PACKET extends Structure {
+	    	// see wintab32.dll specification
+	    	public HDC pkContext;
+	    	public int pkStatus;
+	    	public int pkTime;
+	    	public int pkChanged; 
+	    	public int pkSerialNumber;
+	    	public int pkCursor;
+	    	public int pkButtons;
+	    	public int pkX; 
+	    	public int pkY; 
+	    	public int pkZ; 
+	    	public int pkNormalPressure;
+	    	public int pkTangentPressure;
+	    	public ORIENTATION	pkOrientation;
+	    	public ROTATION pkRotation; 
+
+	    }
+	    
+	    public static class ORIENTATION extends Structure{
+	    	public int orAzimuth;
+	    	public int orAltitude;
+	    	public int orTwist;
+	    }
+		
+		public static class ROTATION extends Structure{
+	    	public int	roPitch;
+	    	public int roRoll;
+	    	public int roYaw;
+	    } 
+
+	}
+
+
+
---
+++ igesture-io/src/main/java/org/ximtec/igesture/io/win32/W32API.java
@@ -0,0 +1,90 @@
+/* Copyright (c) 2007 Timothy Wall, All Rights Reserved
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.  
+ */
+
+package org.ximtec.igesture.io.win32;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.jna.FromNativeContext;
+import com.sun.jna.IntegerType;
+import com.sun.jna.Pointer;
+import com.sun.jna.PointerType;
+import com.sun.jna.win32.StdCallLibrary;
+import com.sun.jna.win32.W32APIFunctionMapper;
+import com.sun.jna.win32.W32APITypeMapper;
+
+/** Base type for most W32 API libraries.  Provides standard options
+ * for unicode/ASCII mappings.  Set the system property <code>w32.ascii</code>
+ * to <code>true</code> to default to the ASCII mappings.
+ */
+public interface W32API extends StdCallLibrary, W32Errors {
+    
+    /** Standard options to use the unicode version of a w32 API. */
+    Map UNICODE_OPTIONS = new HashMap() {
+        {
+            put(OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
+            put(OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
+        }
+    };
+    /** Standard options to use the ASCII/MBCS version of a w32 API. */
+    Map ASCII_OPTIONS = new HashMap() {
+        {
+            put(OPTION_TYPE_MAPPER, W32APITypeMapper.ASCII);
+            put(OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.ASCII);
+        }
+    };
+    Map DEFAULT_OPTIONS = Boolean.getBoolean("w32.ascii") ? ASCII_OPTIONS : UNICODE_OPTIONS;
+    
+    public static class HANDLE extends PointerType { 
+        public HANDLE() { }
+        public HANDLE(Pointer p) { super(p); }
+    };
+    
+    public static class HDC extends HANDLE { }
+    public static class HICON extends HANDLE { }
+    public static class HBITMAP extends HANDLE { }
+    public static class HRGN extends HANDLE { }
+    public static class HWND extends HANDLE { }
+    public static class HINSTANCE extends HANDLE { }
+    public static class HMODULE extends HINSTANCE { }
+    
+
+    HANDLE INVALID_HANDLE_VALUE = new HANDLE() {//Pointer.PM1
+        public void setPointer(Pointer p) { 
+            throw new UnsupportedOperationException("Immutable reference");
+        }
+    };
+    
+    public static class LONG_PTR extends IntegerType { 
+    	public LONG_PTR() { this(0); }
+    	public LONG_PTR(long value) { super(Pointer.SIZE, value); }
+    }
+    public static class LPARAM extends LONG_PTR { 
+    	public LPARAM() { this(0); }
+    	public LPARAM(long value) { super(value); }
+    } 
+    public static class WPARAM extends IntegerType { //IntegerType 
+    	public WPARAM() { this(0); }
+    	public WPARAM(long value) { super(Pointer.SIZE, value); } //super(Pointer.SIZE, value);
+    } 
+    
+    public static class LRESULT extends LONG_PTR { 
+    	public LRESULT() { this(0); }
+    	public LRESULT(long value) { super(value); }
+    } 
+    public static class ULONG_PTR extends IntegerType {
+    	public ULONG_PTR() { super(Pointer.SIZE); }
+    	public ULONG_PTR(long value) { super(Pointer.SIZE, value); }
+    }
+}
