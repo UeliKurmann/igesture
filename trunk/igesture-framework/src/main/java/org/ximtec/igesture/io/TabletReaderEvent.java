714bb59c9136731cdf6ae60b8fd2fdd121634c1c
crocimi <crocimi@f93bbda9-e428-0410-8f24-ec21284828af>
2007-11-15 09:05:26 +0000

    classes TabletReader, TabletReaderEvent, TabletReaderEventListener added

---
+++ trunk/igesture-framework/src/main/java/org/ximtec/igesture/io/TabletReaderEvent.java
@@ -0,0 +1,79 @@
+/*
+ * @(#)TabletReaderEvent.java   1.0   Nov 15, 2007
+ *
+ * Author       :  Michele Croci, mcroci@gmail.com
+ *
+ * Purpose      : 	Tablet reader event.
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Revision Information:
+ *
+ * Date             Who         Reason
+ *
+ * Nov 15, 2007     crocimi     Initial release    
+ * 
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright 1999-2007 ETH Zurich. All Rights Reserved.
+ *
+ * This software is the proprietary information of ETH Zurich.
+ * Use is subject to license terms.
+ * 
+ */
+
+
+package org.ximtec.igesture.io;
+
+import org.sigtec.ink.input.TimestampedLocation;
+import org.sigtec.input.InputDeviceEvent;
+
+
+/**
+ * Tablet reader event.
+ * 
+ * @version 1.0, Nov 2007
+ * @author Michele Croci, mcroci@gmail.com
+ */
+public class TabletReaderEvent implements InputDeviceEvent {
+
+   private TimestampedLocation location;
+
+
+   /**
+    * Constructs a new tablet reader event.
+    * 
+    * @param location the position captured by the tablet.
+    */
+   public TabletReaderEvent(TimestampedLocation location) {
+      this.location = location;
+   }
+
+
+   /**
+    * Returns the time when the event occurred.
+    * 
+    * @return the time when the event occurred.
+    */
+   public long getTimestamp() {
+      return location.getTimestamp();
+   } // getTimestamp
+
+
+   /**
+    * Returns the timestamped location.
+    * 
+    * @return the timestamped location.
+    */
+   public TimestampedLocation getTimestampedLocation() {
+      return location;
+   } // getTimestampedLocation
+   
+   
+   /*
+    * TODO: return others location type,
+    *       provide onther constructors
+    */
+
+}
