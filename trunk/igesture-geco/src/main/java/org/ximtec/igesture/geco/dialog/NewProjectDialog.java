eb4750d3839239237297183f2da463a1011205b3
bsigner <bsigner@f93bbda9-e428-0410-8f24-ec21284828af>
2008-01-15 13:58:35 +0000

    - Updated 'project location'

--- trunk/igesture-geco/src/main/java/org/ximtec/igesture/geco/dialog/NewProjectDialog.java
+++ trunk/igesture-geco/src/main/java/org/ximtec/igesture/geco/dialog/NewProjectDialog.java
@@ -58,258 +58,271 @@ import org.ximtec.igesture.geco.xml.XMLGeco;
 import org.ximtec.igesture.graphics.SwingTool;
 
 
-
 /**
- *  New project dialog
- *  
+ * New project dialog
+ * 
  * @version 0.9, Dec 3, 2007
  * @author Michele Croci, mcroci@gmail.com
  */
-public class NewProjectDialog extends BasicDialog{
-   
-   
+public class NewProjectDialog extends BasicDialog {
+
    private MainView view;
-   
+
    private int DIALOG_WIDTH = 700;
    private int DIALOG_HEIGHT = 300;
-   
-   private static String XML_EXTENSION = "xml";
-   private  BasicTextField fileTextField;
-   private  BasicTextField projectTextField;
-   private JButton createButton;
-   private String filePath="";
 
-   
-   public NewProjectDialog(MainView view){
-      this.view=view;
+   private static String XML_EXTENSION = "xml";
+   private BasicTextField fileTextField;
+   private BasicTextField projectTextField;
+   private JButton createButton;
+   private String filePath = "";
+
+
+   public NewProjectDialog(MainView view) {
+      this.view = view;
       setModal(true);
       init();
-   }//NewProjectDialog
-   
+   }// NewProjectDialog
+
+
    /**
     * Inits the dialog.
     */
-   private void init(){
+   private void init() {
       File[] roots = File.listRoots();
-      filePath = System.getProperty ("user.home")+"\\";
+      filePath = System.getProperty("user.home") + "\\";
       JPanel mainPanel = new JPanel();
-      mainPanel.setBorder(new TitledBorder(new BevelBorder(0,Color.gray,Color.gray), Constant.NEW_PROJECT_DIALOG_TITLE));
-     
-      
+      mainPanel.setBorder(new TitledBorder(new BevelBorder(0, Color.gray,
+            Color.gray), Constant.NEW_PROJECT_DIALOG_TITLE));
+
       this.setTitle(Constant.NEW_PROJECT_DIALOG_TITLE);
       mainPanel.setLayout(new GridBagLayout());
       this.setLayout(new GridBagLayout());
-      Point p = new Point(view.getLocation().x+50, view.getLocation().y+100);
+      Point p = new Point(view.getLocation().x + 50, view.getLocation().y + 100);
       this.setLocation(p);
-      this.setSize(new Dimension( DIALOG_WIDTH, DIALOG_HEIGHT ));
+      this.setSize(new Dimension(DIALOG_WIDTH, DIALOG_HEIGHT));
 
-      mainPanel.add(GuiTool.createLabel(Constant.PROJECT_NAME),  new GridBagConstraints(0,0,1,1,0,0, GridBagConstraints.CENTER, GridBagConstraints.NONE,
-            new Insets(10,10,10,10),0,0 ) );
-      
-      projectTextField = GuiTool.createTextField(Constant.PROJECT_NAME_TEXT_FIELD);
-      projectTextField.getDocument().addDocumentListener(new MyDocumentListener());
+      mainPanel.add(GuiTool.createLabel(Constant.PROJECT_NAME),
+            new GridBagConstraints(0, 0, 1, 1, 0, 0, GridBagConstraints.CENTER,
+                  GridBagConstraints.NONE, new Insets(10, 10, 10, 10), 0, 0));
+
+      projectTextField = GuiTool
+            .createTextField(Constant.PROJECT_NAME_TEXT_FIELD);
+      projectTextField.getDocument().addDocumentListener(
+            new MyDocumentListener());
       projectTextField.setText("");
-      mainPanel.add(projectTextField,  
-            new GridBagConstraints(1,0,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-            new Insets(10,10,10,10),0,0 ) );
-      
-      
-      mainPanel.add(GuiTool.createLabel(Constant.PROJECT_FILE),  new GridBagConstraints(0,1,1,1,0,0, GridBagConstraints.CENTER, GridBagConstraints.NONE,
-            new Insets(10,10,10,10),0,0 ) );
-      
+      mainPanel.add(projectTextField, new GridBagConstraints(1, 0, 1, 1, 1, 1,
+            GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+            new Insets(10, 10, 10, 10), 0, 0));
+
+      mainPanel.add(GuiTool.createLabel(Constant.PROJECT_LOCATION),
+            new GridBagConstraints(0, 1, 1, 1, 0, 0, GridBagConstraints.CENTER,
+                  GridBagConstraints.NONE, new Insets(10, 10, 10, 10), 0, 0));
+
       fileTextField = GuiTool.createTextField(Constant.PROJECT_FILE_TEXT_FIELD);
       fileTextField.setEditable(false);
       fileTextField.setText(filePath);
-      mainPanel.add(fileTextField,  
-            new GridBagConstraints(1,1,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
-            new Insets(10,10,10,10),0,0 ) );
-      
+      mainPanel.add(fileTextField, new GridBagConstraints(1, 1, 1, 1, 1, 1,
+            GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL,
+            new Insets(10, 10, 10, 10), 0, 0));
+
       JButton browseButton = SwingTool.createButton(Constant.BROWSE);
       browseButton.addActionListener(new BrowseListener());
-      mainPanel.add(browseButton,  
-            new GridBagConstraints(2,1,1,1,0,0, GridBagConstraints.CENTER, GridBagConstraints.NONE,
-            new Insets(10,10,10,10),0,0 ) );
-      
+      mainPanel.add(browseButton, new GridBagConstraints(2, 1, 1, 1, 0, 0,
+            GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10,
+                  10, 10, 10), 0, 0));
+
       createButton = SwingTool.createButton(Constant.CREATE);
       createButton.setEnabled(false);
       createButton.addActionListener(new CreateListener());
       JButton cancelButton = SwingTool.createButton(Constant.CANCEL);
       cancelButton.addActionListener(new CancelListener());
-      
+
       JPanel buttonPanel = new JPanel();
-      buttonPanel.setLayout(new GridLayout(1,1, 20, 0));
+      buttonPanel.setLayout(new GridLayout(1, 1, 20, 0));
       buttonPanel.add(createButton);
       buttonPanel.add(cancelButton);
-      mainPanel.add(buttonPanel,  
-            new GridBagConstraints(0,3,3,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.NONE,
-            new Insets(10,10,10,10),0,0 ) );
-  
-      this.add(mainPanel,
-            new GridBagConstraints(0,3,3,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-                  new Insets(20,20,20,20),0,0 ) );
-   }//init
-   
-   
+      mainPanel.add(buttonPanel, new GridBagConstraints(0, 3, 3, 1, 1, 1,
+            GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10,
+                  10, 10, 10), 0, 0));
+
+      this.add(mainPanel, new GridBagConstraints(0, 3, 3, 1, 1, 1,
+            GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(20,
+                  20, 20, 20), 0, 0));
+   }// init
+
+
    /**
     * Shows the dialog.
     */
-   public void showDialog(){
+   public void showDialog() {
       reset();
       setVisible(true);
-   }//showDialog
-   
+   }// showDialog
+
+
    /**
     * Resets the dialog.
     */
-   public void reset(){
+   public void reset() {
       fileTextField.setText(filePath);
       projectTextField.setText("");
-   }//reset
-   
-   
+   }// reset
+
    /**
     * Listener for browse event.
     */
-   private class BrowseListener implements ActionListener{
-      public void actionPerformed(ActionEvent e){
-         //display file chooser dialog
+   private class BrowseListener implements ActionListener {
+
+      public void actionPerformed(ActionEvent e) {
+         // display file chooser dialog
 
          JFileChooser fileChooser = new JFileChooser();
          fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-         int status = fileChooser.showDialog(null,"Open");
+         int status = fileChooser.showDialog(null, "Open");
          if (status == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
-            if(selectedFile != null){
-               
-                String fileName = "";
-                 if (selectedFile.getAbsolutePath().charAt(selectedFile.getAbsolutePath().length()-1)!='\\'){
-                      fileName = selectedFile.getAbsolutePath()+"\\"+
-                      NewProjectDialog.this.projectTextField.getText();
-                      if(!NewProjectDialog.this.projectTextField.getText().equals("")){
-                         fileName+="."+XML_EXTENSION;
-                      }
-                      filePath=selectedFile.getAbsolutePath()+"\\";
-                   }
-                   else{
-                      fileName = selectedFile.getAbsolutePath()+
-                      NewProjectDialog.this.projectTextField.getText();
-                      if(!NewProjectDialog.this.projectTextField.getText().equals("")){
-                         fileName+="."+XML_EXTENSION;
-                      }
-                      filePath=selectedFile.getAbsolutePath();
-                   }
-                fileTextField.setText(fileName);
+            if (selectedFile != null) {
+
+               String fileName = "";
+               if (selectedFile.getAbsolutePath().charAt(
+                     selectedFile.getAbsolutePath().length() - 1) != '\\') {
+                  fileName = selectedFile.getAbsolutePath() + "\\"
+                        + NewProjectDialog.this.projectTextField.getText();
+                  if (!NewProjectDialog.this.projectTextField.getText().equals(
+                        "")) {
+                     fileName += "." + XML_EXTENSION;
+                  }
+                  filePath = selectedFile.getAbsolutePath() + "\\";
+               }
+               else {
+                  fileName = selectedFile.getAbsolutePath()
+                        + NewProjectDialog.this.projectTextField.getText();
+                  if (!NewProjectDialog.this.projectTextField.getText().equals(
+                        "")) {
+                     fileName += "." + XML_EXTENSION;
+                  }
+                  filePath = selectedFile.getAbsolutePath();
+               }
+               fileTextField.setText(fileName);
             }
-         } else if (status == JFileChooser.CANCEL_OPTION) {
-           
+         }
+         else if (status == JFileChooser.CANCEL_OPTION) {
+
          }
 
       }
    }
-   
-   
+
    /**
     * Listener for create project event.
     */
-   private class CreateListener implements ActionListener{
-      public void actionPerformed(ActionEvent e){
-      
-         if((!fileTextField.getText().equals(""))&& (!projectTextField.getText().equals(""))){
-            String fileName= fileTextField.getText();
+   private class CreateListener implements ActionListener {
 
-            boolean ok=true;
+      public void actionPerformed(ActionEvent e) {
+
+         if ((!fileTextField.getText().equals(""))
+               && (!projectTextField.getText().equals(""))) {
+            String fileName = fileTextField.getText();
+
+            boolean ok = true;
             File temp = new File(fileName);
-            if (temp.exists()){
-               int n = JOptionPane.showConfirmDialog(
-                     NewProjectDialog.this,
-                     Constant.OVERWRITE_FILE,
-                     "",
-                     JOptionPane.YES_NO_OPTION);
-               if (n!=0){
-                  ok=false;
+            if (temp.exists()) {
+               int n = JOptionPane.showConfirmDialog(NewProjectDialog.this,
+                     Constant.OVERWRITE_FILE, "", JOptionPane.YES_NO_OPTION);
+               if (n != 0) {
+                  ok = false;
                }
             }
-              
-            if(ok){
-               //update model
+
+            if (ok) {
+               // update model
                File gsFile;
                try {
-                  gsFile = new File(ClassLoader.getSystemResource(MainModel.GESTURE_SET).toURI());
-               } catch(URISyntaxException ex) {
-                  gsFile = new File(ClassLoader.getSystemResource(MainModel.GESTURE_SET).getPath());
+                  gsFile = new File(ClassLoader.getSystemResource(
+                        MainModel.GESTURE_SET).toURI());
                }
-               
+               catch (URISyntaxException ex) {
+                  gsFile = new File(ClassLoader.getSystemResource(
+                        MainModel.GESTURE_SET).getPath());
+               }
+
                GestureSet gestureSet = XMLGeco.importGestureSet(gsFile).get(0);
-             
-               
-               
-       //        GestureSet gestureSet = XMLGeco.importGestureSet(
-         //            new File(ClassLoader.getSystemResource(GecoMainModel.GESTURE_SET).getFile())).get(0);
- 
+
+               // GestureSet gestureSet = XMLGeco.importGestureSet(
+               // new
+               // File(ClassLoader.getSystemResource(GecoMainModel.GESTURE_SET).getFile())).get(0);
+
                view.getModel().clearData();
                view.getModel().initRecogniser(gestureSet);
                view.getModel().loadGestureSet(gestureSet);
-               
+
                view.getModel().setProjectFile(temp);
-               view.getModel().setGestureSetFileName(view.getModel().GESTURE_SET);
+               view.getModel()
+                     .setGestureSetFileName(view.getModel().GESTURE_SET);
                view.getModel().setProjectName(projectTextField.getText());
-               
-               
-               //update view
+
+               // update view
                view.initProjectView(projectTextField.getText());
                view.enableMenuItem();
                view.updateGestureList();
                NewProjectDialog.this.dispose();
-               
-               //save
+
+               // save
                (new SaveProjectAction(view)).save();
             }
          }
-         
-         
-      }//actionPerformed
-      
+
+      }// actionPerformed
+
    }
-   
-   
+
    /**
     * Listener for cancel event.
     */
-   private class CancelListener implements ActionListener{
-      public void actionPerformed(ActionEvent e){
+   private class CancelListener implements ActionListener {
+
+      public void actionPerformed(ActionEvent e) {
          NewProjectDialog.this.dispose();
-      }//actionPerformed
-      
+      }// actionPerformed
+
    }
-   
-   
+
    /**
     * Listener for project name edit event.
     */
    private class MyDocumentListener implements DocumentListener {
-   
+
       public void insertUpdate(DocumentEvent e) {
          update();
-     }
-     public void removeUpdate(DocumentEvent e) {
-         update();
-     }
-     public void changedUpdate(DocumentEvent e) {
-         //Plain text components do not fire these events
-     }
+      }
 
-      public void update(){
-         if((!projectTextField.getText().equals(""))){
+
+      public void removeUpdate(DocumentEvent e) {
+         update();
+      }
+
+
+      public void changedUpdate(DocumentEvent e) {
+         // Plain text components do not fire these events
+      }
+
+
+      public void update() {
+         if ((!projectTextField.getText().equals(""))) {
             NewProjectDialog.this.createButton.setEnabled(true);
-            NewProjectDialog.this.fileTextField.setText(NewProjectDialog.this.filePath+
-                  NewProjectDialog.this.projectTextField.getText()+"."+XML_EXTENSION);
+            NewProjectDialog.this.fileTextField
+                  .setText(NewProjectDialog.this.filePath
+                        + NewProjectDialog.this.projectTextField.getText() + "."
+                        + XML_EXTENSION);
          }
-         else{
+         else {
             NewProjectDialog.this.createButton.setEnabled(false);
-         NewProjectDialog.this.fileTextField.setText(NewProjectDialog.this.filePath+
-               NewProjectDialog.this.projectTextField.getText());
+            NewProjectDialog.this.fileTextField
+                  .setText(NewProjectDialog.this.filePath
+                        + NewProjectDialog.this.projectTextField.getText());
          }
-      
-      }//update
+
+      }// update
    }
 }
