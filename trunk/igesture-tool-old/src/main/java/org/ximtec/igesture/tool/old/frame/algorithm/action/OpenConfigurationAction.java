8a848e590c97731e908c1ee1efb2e3ca7f18f144
bsigner <bsigner@f93bbda9-e428-0410-8f24-ec21284828af>
2008-03-18 16:53:58 +0000

    - Added folder

---
+++ trunk/igesture-tool-old/src/main/java/org/ximtec/igesture/tool/old/frame/algorithm/action/OpenConfigurationAction.java
@@ -0,0 +1,63 @@
+/*
+ * @(#)OpenConfigurationAction.java	1.0   Dec 4, 2006
+ *
+ * Author		:	Ueli Kurmann, kurmannu@ethz.ch
+ *
+ * Purpose		: 	Opens a configuration.
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Revision Information:
+ *
+ * Date				Who			Reason
+ *
+ * Dec 04, 2006     ukurmann    Initial Release
+ * Mar 24, 2007     bsigner     Cleanup
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright 1999-2008 ETH Zurich. All Rights Reserved.
+ *
+ * This software is the proprietary information of ETH Zurich.
+ * Use is subject to license terms.
+ * 
+ */
+
+
+package org.ximtec.igesture.tool.old.frame.algorithm.action;
+
+import java.awt.event.ActionEvent;
+
+import org.sigtec.graphix.widget.BasicAction;
+import org.ximtec.igesture.tool.old.GestureToolMain;
+import org.ximtec.igesture.tool.old.frame.algorithm.AlgorithmConfiguration;
+
+
+/**
+ * Opens a configuration.
+ * 
+ * @version 1.0, Dec 2006
+ * @author Ueli Kurmann, kurmannu@ethz.ch
+ * @author Beat Signer, signer@inf.ethz.ch
+ */
+public class OpenConfigurationAction extends BasicAction {
+
+   /**
+    * The key used to retrieve action details from the resource bundle.
+    */
+   public static final String KEY = "ConfigOpenAction";
+
+   private AlgorithmConfiguration algorithmConfiguration;
+
+
+   public OpenConfigurationAction(AlgorithmConfiguration algorithmConfiguration) {
+      super(KEY, GestureToolMain.getGuiBundle());
+      this.algorithmConfiguration = algorithmConfiguration;
+   }
+
+
+   public void actionPerformed(ActionEvent arg0) {
+      algorithmConfiguration.openSelectedConfiguration();
+   } // actionPerformed
+
+}
