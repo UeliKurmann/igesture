8c1e6253356f333cfd97d270b7bf8f9a74aeb6f6
kurmannu <1230897+UeliKurmann@users.noreply.github.com>
2008-03-19 23:24:43 +0000

    iGesture Tool Prototyping

--- igesture-tool/src/main/java/org/ximtec/igesture/tool/view/admin/panel/GestureClassPanel.java
+++ igesture-tool/src/main/java/org/ximtec/igesture/tool/view/admin/panel/GestureClassPanel.java
@@ -6,6 +6,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import org.ximtec.igesture.core.Descriptor;
 import org.ximtec.igesture.core.GestureClass;
 import org.ximtec.igesture.tool.explorer.DefaultExplorerTreeView;
 
@@ -25,16 +26,26 @@ public class GestureClassPanel extends DefaultExplorerTreeView {
 
 		builder.append(new JLabel("GestureClass " + gestureClass.getName()));
 		builder.nextLine(4);
-		
+
 		builder.append(new JLabel("Name"));
 		JTextField textField = new JTextField();
 		textField.setText(gestureClass.getName());
 		builder.append(textField);
 		builder.nextLine(2);
-		
+
+		builder.append(new JLabel("Number of Descriptors"));
+		builder.append(new JLabel(Integer.toString(gestureClass
+				.getDescriptors().size())));
+		builder.nextLine(2);
+		for (Descriptor descriptor : gestureClass.getDescriptors()) {
+			builder.append(new JLabel("Descriptor Name"));
+			builder.append(new JLabel(descriptor.getType().getName()));
+			builder.nextLine(2);
+		}
+
 		JPanel panel = builder.getPanel();
 		panel.setOpaque(false);
-		
+
 		this.add(panel);
 
 		this.setOpaque(true);
