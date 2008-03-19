b943b6d0c5828f28e7237fea6e4d9a991f8faafb
kurmannu <1230897+UeliKurmann@users.noreply.github.com>
2008-03-19 23:17:26 +0000

    iGesture Tool Prototyping

--- igesture-tool/src/main/java/org/ximtec/igesture/tool/explorer/ExplorerTreeListener.java
+++ igesture-tool/src/main/java/org/ximtec/igesture/tool/explorer/ExplorerTreeListener.java
@@ -2,10 +2,8 @@ package org.ximtec.igesture.tool.explorer;
 
 import javax.swing.event.TreeModelEvent;
 import javax.swing.event.TreeModelListener;
-import javax.swing.event.TreeSelectionEvent;
-import javax.swing.event.TreeSelectionListener;
 
-public class ExplorerTreeListener implements TreeModelListener, TreeSelectionListener{
+public class ExplorerTreeListener implements TreeModelListener{
 
 	@Override
 	public void treeNodesChanged(TreeModelEvent arg0) {
@@ -28,12 +26,6 @@ public class ExplorerTreeListener implements TreeModelListener, TreeSelectionLis
 	@Override
 	public void treeStructureChanged(TreeModelEvent arg0) {
 		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void valueChanged(TreeSelectionEvent e) {
-		System.out.println(e.getPaths());
 		
 	}
 
