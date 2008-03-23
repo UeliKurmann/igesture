ed59701efd94661ef8b73c357d06a6e647fb99b6
kurmannu <1230897+UeliKurmann@users.noreply.github.com>
2008-03-23 17:06:49 +0000

    - Add File Headers - Reformatting

--- igesture-tool/src/main/java/org/ximtec/igesture/tool/Main.java
+++ igesture-tool/src/main/java/org/ximtec/igesture/tool/Main.java
@@ -1,21 +1,48 @@
+/*
+ * @(#)$Id:$
+ *
+ * Author   : Ueli Kurmann, ueli@smartness.ch
+ *                  
+ *                  
+ *
+ * Purpose    : 
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Revision Information:
+ *
+ * Date       Who     Reason
+ *
+ * 23.03.2008   ukurmann  Initial Release
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright 1999-2008 ETH Zurich. All Rights Reserved.
+ *
+ * This software is the proprietary information of ETH Zurich.
+ * Use is subject to license terms.
+ * 
+ */
+
 package org.ximtec.igesture.tool;
 
 import javax.swing.UIManager;
 
 import org.ximtec.igesture.tool.view.MainController;
 
+
 public class Main {
-	
-	public static void main(String[] args){
-	
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		new MainController();
-		
-		
-	}
+
+   public static void main(String[] args) {
+
+      try {
+         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+      }
+      catch (Exception e) {
+         e.printStackTrace();
+      }
+      new MainController();
+
+   }
 
 }
