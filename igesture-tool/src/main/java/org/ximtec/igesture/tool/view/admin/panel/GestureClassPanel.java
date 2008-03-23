0215f5a8b6936ff022c79ca2fa2bc93c8e8790c0
kurmannu <1230897+UeliKurmann@users.noreply.github.com>
2008-03-23 17:23:35 +0000

    - Add File Headers - Reformatting

--- igesture-tool/src/main/java/org/ximtec/igesture/tool/view/admin/panel/GestureClassPanel.java
+++ igesture-tool/src/main/java/org/ximtec/igesture/tool/view/admin/panel/GestureClassPanel.java
@@ -1,4 +1,27 @@
-
+/*
+ * @(#)$Id$
+ *
+ * Author   : Ueli Kurmann, igesture@uelikurmann.ch
+ *                                   
+ *                                   
+ * Purpose  : 
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Revision Information:
+ *
+ * Date       Who     Reason
+ *
+ * 23.03.2008 ukurmann  Initial Release
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright 1999-2008 ETH Zurich. All Rights Reserved.
+ *
+ * This software is the proprietary information of ETH Zurich.
+ * Use is subject to license terms.
+ * 
+ */
 
 package org.ximtec.igesture.tool.view.admin.panel;
 
--- igesture-tool/src/main/java/org/ximtec/igesture/tool/view/admin/panel/GestureSetsPanel.java
+++ igesture-tool/src/main/java/org/ximtec/igesture/tool/view/admin/panel/GestureSetsPanel.java
@@ -1,4 +1,27 @@
-
+/*
+ * @(#)$Id: Main.java 454 2008-03-23 17:06:49Z kurmannu $
+ *
+ * Author   : Ueli Kurmann, igesture@uelikurmann.ch
+ *                                   
+ *                                   
+ * Purpose  : 
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Revision Information:
+ *
+ * Date       Who     Reason
+ *
+ * 23.03.2008 ukurmann  Initial Release
+ *
+ * -----------------------------------------------------------------------
+ *
+ * Copyright 1999-2008 ETH Zurich. All Rights Reserved.
+ *
+ * This software is the proprietary information of ETH Zurich.
+ * Use is subject to license terms.
+ * 
+ */
 
 package org.ximtec.igesture.tool.view.admin.panel;
 
