0d6973e8f6a58a484a0354027d78dbc8f0e6752e
kurmannu <1230897+UeliKurmann@users.noreply.github.com>
2008-05-03 08:18:54 +0000

    Bugfixes JUnit Test (Rename Features)

--- igesture-framework/src/test/java/org/ximtec/igesture/framework/algorithm/feature/test/FeatureTest.java
+++ igesture-framework/src/test/java/org/ximtec/igesture/framework/algorithm/feature/test/FeatureTest.java
@@ -46,145 +46,145 @@ public class FeatureTest {
 
 	@org.junit.Test
 	public void feature1() {
-		org.junit.Assert.assertEquals(compute(F1.class), -0.2425356338943292,
+		org.junit.Assert.assertEquals(-0.2425356338943292,compute(F1.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature2() {
-		org.junit.Assert.assertEquals(compute(F2.class), -0.9701424979308327,
+		org.junit.Assert.assertEquals( -0.9701424979308327,compute(F2.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature3() {
-		org.junit.Assert.assertEquals(compute(F3.class), 235.79456557493563,
+		org.junit.Assert.assertEquals( 235.79456557493563,compute(F3.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature4() {
-		org.junit.Assert.assertEquals(compute(F4.class), 1.0125700761060759,
+		org.junit.Assert.assertEquals( 1.0125700761060759,compute(F4.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature5() {
-		org.junit.Assert.assertEquals(compute(F5.class), 120.97410570093801,
+		org.junit.Assert.assertEquals(120.97410570093801,compute(F5.class), 
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature6() {
-		org.junit.Assert.assertEquals(compute(F6.class), 0.9946917938265513,
+		org.junit.Assert.assertEquals( 0.9946917938265513,compute(F6.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature7() {
-		org.junit.Assert.assertEquals(compute(F7.class), -0.10289915108550529,
+		org.junit.Assert.assertEquals( -0.10289915108550529,compute(F7.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature8() {
-		org.junit.Assert.assertEquals(compute(F8.class), 469.8368105034607,
+		org.junit.Assert.assertEquals( 469.8368105034607,compute(F8.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature9() {
-		org.junit.Assert.assertEquals(compute(F9.class), 1.240849226681367,
+		org.junit.Assert.assertEquals( 1.240849226681367,compute(F9.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature10() {
-		org.junit.Assert.assertEquals(compute(F10.class), 7.565923162004228,
+		org.junit.Assert.assertEquals( 7.565923162004228,compute(F10.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature11() {
-		org.junit.Assert.assertEquals(compute(F11.class), 3.054971844940879,
+		org.junit.Assert.assertEquals(3.054971844940879,compute(F11.class), 
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature12() {
-		org.junit.Assert.assertEquals(compute(F12.class), 1.5114150572791722,
+		org.junit.Assert.assertEquals( 1.5114150572791722,compute(F12.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature13() {
-		org.junit.Assert.assertEquals(compute(F13.class), 841.0, 0.0000005);
+		org.junit.Assert.assertEquals( 841.0,compute(F13.class), 0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature14() {
-		org.junit.Assert.assertEquals(compute(F14.class), 3.883779985652067,
+		org.junit.Assert.assertEquals(2.0 ,compute(F14.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature15() {
-		org.junit.Assert.assertEquals(compute(F15.class), 2.0, 0.0000005);
+		org.junit.Assert.assertEquals(0.45247292600776656 , compute(F15.class),0.0000005);
 	}
-
+	
 	@org.junit.Test
 	public void feature16() {
-		org.junit.Assert.assertEquals(compute(F16.class), 0.45247292600776656,
+		org.junit.Assert.assertEquals(0.8672841925557736 ,compute(F16.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature17() {
-		org.junit.Assert.assertEquals(compute(F17.class), 0.8869103272628072,
+		org.junit.Assert.assertEquals(0.539242596680645 ,compute(F17.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature18() {
-		org.junit.Assert.assertEquals(compute(F18.class), 0.8672841925557736,
+		org.junit.Assert.assertEquals(0.4619416320 ,compute(F18.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature19() {
-		org.junit.Assert.assertEquals(compute(F19.class), 0.539242596680645,
+		org.junit.Assert.assertEquals(0.5130487439605235,compute(F19.class), 
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature20() {
-		org.junit.Assert.assertEquals(compute(F20.class), 0.5130487439605235,
+		org.junit.Assert.assertEquals(1.0 ,compute(F20.class),
 				0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature21() {
-		org.junit.Assert.assertEquals(compute(F21.class), 1.0, 0.0000005);
+		org.junit.Assert.assertEquals( 3.883779985652067, compute(F21.class),0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature22() {
-		org.junit.Assert.assertEquals(compute(F22.class), 0.0, 0.0000005);
+		org.junit.Assert.assertEquals( 0.0, compute(F22.class),0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature23() {
-		org.junit.Assert.assertEquals(compute(F23.class), 0.0, 0.0000005);
+		org.junit.Assert.assertEquals( 0.0, compute(F23.class),0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature24() {
-		org.junit.Assert.assertEquals(compute(F24.class), 1.0, 0.0000005);
+		org.junit.Assert.assertEquals( 1.0, compute(F24.class),0.0000005);
 	}
 
 	@org.junit.Test
 	public void feature25() {
-		org.junit.Assert.assertEquals(compute(F25.class), 1.0, 0.0000005);
+		org.junit.Assert.assertEquals(1.0, compute(F25.class), 0.0000005);
 	}
 	
 	@org.junit.Test
