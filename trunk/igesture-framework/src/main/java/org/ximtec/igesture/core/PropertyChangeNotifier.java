9405d09e8f8e8691f0e9a3888a97b76cfd2d7736
bsigner <bsigner@f93bbda9-e428-0410-8f24-ec21284828af>
2008-09-25 21:15:22 +0000

    - Cleanup

--- trunk/igesture-framework/src/main/java/org/ximtec/igesture/core/PropertyChangeNotifier.java
+++ trunk/igesture-framework/src/main/java/org/ximtec/igesture/core/PropertyChangeNotifier.java
@@ -1,18 +1,20 @@
 /*
- * @(#)$Id:$
+ * @(#)PropertyChangeNotifier.java   1.0   Mar 25, 2008
  *
  * Author		:	Ueli Kurmann, igesture@uelikurmann.ch
  *                  
  *
- * Purpose		: 
+ * Purpose		:   Interface to be implemented by classes that offer a
+ *                  property change notification.
  *
  * -----------------------------------------------------------------------
  *
  * Revision Information:
  *
- * Date				Who			Reason
+ * Date             Who         Reason
  *
- * 25.03.2008			ukurmann	Initial Release
+ * 25.03.2008		ukurmann	Initial Release
+ * 25.09.2008       bsigner     Cleanup
  *
  * -----------------------------------------------------------------------
  *
@@ -29,39 +31,47 @@ package org.ximtec.igesture.core;
 import java.beans.PropertyChangeListener;
 
 
-
 /**
- * Comment
+ * Interface to be implemented by classes that offer a property change
+ * notification.
+ * 
  * @version 1.0 25.03.2008
  * @author Ueli Kurmann
+ * @author Beat Signer, signer@inf.ethz.ch
  */
 public interface PropertyChangeNotifier {
+
    /**
-    * Adds a property change listener
-    * @param listener
+    * Adds a property change listener.
+    * @param listener the property change listener to be added.
     */
    public void addPropertyChangeListener(PropertyChangeListener listener);
 
-   /**
-    * Adds a property change listener
-    * @param propertyName
-    * @param listener
-    */
-   public void addPropertyChangeListener(String propertyName,
-           PropertyChangeListener listener);
 
    /**
-    * Removes a property change listener
-    * @param listener
+    * Adds a property change listener
+    * @param propertyName the name of the property to be tracked by the property
+    *            change listener.
+    * @param listener the property change listener to be added.
+    */
+   public void addPropertyChangeListener(String propertyName,
+         PropertyChangeListener listener);
+
+
+   /**
+    * Removes a property change listener.
+    * @param listener the property change listener to be removed.
     */
    public void removePropertyChangeListener(PropertyChangeListener listener);
 
+
    /**
-    * Removes a property change listener
-    * @param propertyName
+    * Removes a property change listener.
+    * @param propertyName the name of the property for which the listener has to
+    *            be removed.
     * @param listener
     */
    public void removePropertyChangeListener(String propertyName,
-           PropertyChangeListener listener);
+         PropertyChangeListener listener);
 
 }
