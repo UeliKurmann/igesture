b651258be991662790ca7a45c2b715589dc69932
kurmannu <kurmannu@f93bbda9-e428-0410-8f24-ec21284828af>
2008-12-09 19:28:11 +0000

    Add Comments

--- trunk/igesture-framework/src/main/java/org/ximtec/igesture/io/GestureDevice.java
+++ trunk/igesture-framework/src/main/java/org/ximtec/igesture/io/GestureDevice.java
@@ -38,18 +38,51 @@ import org.ximtec.igesture.core.Gesture;
  */
 public interface GestureDevice<E, F> {
 
+   /**
+    * Initializes the device. After executing this method, gestures can be
+    * captured.
+    */
    void init();
 
+
+   /**
+    * Disposes the device. After executing this method, gestures are not captured
+    * anymore and all dependent resources are released.
+    */
    void dispose();
 
+
+   /**
+    * Returns the gestures. This method should block, if no gesture is available. 
+    * @return the gesture. 
+    */
    Gesture<E> getGesture();
-   
+
+
+   /**
+    * Deletes the current gesture. 
+    */
    void clear();
 
+
+   /**
+    * Returns chunks of a gesture while drawing it. 
+    * @return chunks of a gesture. 
+    */
    List<F> getChunks();
 
+
+   /**
+    * Add a gesture handler
+    * @param listener
+    */
    void addGestureHandler(GestureEventListener listener);
 
+
+   /**
+    * Remove a gesture handler
+    * @param listener
+    */
    void removeGestureHandle(GestureEventListener listener);
 
 }
