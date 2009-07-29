a58999f9114eed900304329513c1cfa6e889f786
kurmannu <1230897+UeliKurmann@users.noreply.github.com>
2009-07-29 20:03:00 +0000

    - execTabChanged - separator in context menu (nodeinfo) - saveFlag

--- igesture-tool/src/main/java/org/ximtec/igesture/tool/view/MainView.java
+++ igesture-tool/src/main/java/org/ximtec/igesture/tool/view/MainView.java
@@ -23,7 +23,6 @@
  * 
  */
 
-
 package org.ximtec.igesture.tool.view;
 
 import java.awt.Frame;
@@ -31,15 +30,17 @@ import java.awt.Frame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JTabbedPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.sigtec.graphix.widget.BasicMenu;
 import org.sigtec.graphix.widget.BasicMenuItem;
 import org.ximtec.igesture.tool.GestureConstants;
+import org.ximtec.igesture.tool.core.Command;
 import org.ximtec.igesture.tool.core.Controller;
 import org.ximtec.igesture.tool.core.DefaultView;
 import org.ximtec.igesture.tool.core.TabbedView;
 import org.ximtec.igesture.tool.service.GuiBundleService;
-
 
 /**
  * The main view of the iGesture tool.
@@ -51,72 +52,71 @@ import org.ximtec.igesture.tool.service.GuiBundleService;
 @SuppressWarnings("serial")
 public class MainView extends DefaultView {
 
-   private JTabbedPane tabbedPane;
-   private JMenuBar menuBar;
+  private JTabbedPane tabbedPane;
+  private JMenuBar menuBar;
 
+  public MainView(Controller controller) {
+    super(controller);
+    initMenu();
+    initView();
+  }
 
-   public MainView(Controller controller) {
-      super(controller);
-      initMenu();
-      initView();
-   }
+  private void initView() {
+    setBounds(100, 100, 900, 650);
+    setDefaultCloseOperation(Frame.NORMAL);
+    setTitle(getComponentFactory().getGuiBundle().getName(GestureConstants.APPLICATION_ROOT));
+    setIconImage(getComponentFactory().getGuiBundle().getSmallIcon(GestureConstants.APPLICATION_ROOT).getImage());
+    setVisible(true);
+    tabbedPane = new JTabbedPane();
+    this.add(tabbedPane);
 
+    tabbedPane.addChangeListener(new ChangeListener() {
 
-   private void initView() {
-      setBounds(100, 100, 900, 650);
-      setDefaultCloseOperation(Frame.NORMAL);
-      setTitle(getComponentFactory().getGuiBundle().getName(
-            GestureConstants.APPLICATION_ROOT));
-      setIconImage(getComponentFactory().getGuiBundle().getSmallIcon(
-            GestureConstants.APPLICATION_ROOT).getImage());
-      setVisible(true);
-      tabbedPane = new JTabbedPane();
-      this.add(tabbedPane);
-   }
+      @Override
+      public void stateChanged(ChangeEvent arg0) {
+        getController().execute(new Command(MainController.CMD_CHANGE_TAB, this));
 
+      }
 
-   private void initMenu() {
-      menuBar = new JMenuBar();
-      setJMenuBar(menuBar);
+    });
+  }
 
-      JMenu fileMenu = new BasicMenu(GestureConstants.FILE_MENU, getController()
-            .getLocator().getService(GuiBundleService.IDENTIFIER,
-                  GuiBundleService.class));
-      menuBar.add(fileMenu);
+  private void initMenu() {
+    menuBar = new JMenuBar();
+    setJMenuBar(menuBar);
 
-      JMenu helpMenu = new BasicMenu(GestureConstants.HELP_MENU, getController()
-            .getLocator().getService(GuiBundleService.IDENTIFIER,
-                  GuiBundleService.class));
-      menuBar.add(helpMenu);
+    JMenu fileMenu = new BasicMenu(GestureConstants.FILE_MENU, getController().getLocator().getService(
+        GuiBundleService.IDENTIFIER, GuiBundleService.class));
+    menuBar.add(fileMenu);
 
-      fileMenu.add(createMenuItem(MainController.CMD_LOAD));
-      fileMenu.add(createMenuItem(MainController.CMD_CLOSE_WS));
-      fileMenu.addSeparator();
-      fileMenu.add(createMenuItem(MainController.CMD_SAVE));
-      fileMenu.addSeparator();
-      fileMenu.add(createMenuItem(MainController.CMD_EXIT));
+    JMenu helpMenu = new BasicMenu(GestureConstants.HELP_MENU, getController().getLocator().getService(
+        GuiBundleService.IDENTIFIER, GuiBundleService.class));
+    menuBar.add(helpMenu);
 
-      BasicMenuItem aboutItem = new BasicMenuItem();
-      aboutItem.setAction(getController().getAction(
-            MainController.CMD_SHOW_ABOUT_DIALOG));
-      helpMenu.add(aboutItem);
-   }
+    fileMenu.add(createMenuItem(MainController.CMD_LOAD));
+    fileMenu.add(createMenuItem(MainController.CMD_CLOSE_WS));
+    fileMenu.addSeparator();
+    fileMenu.add(createMenuItem(MainController.CMD_SAVE));
+    fileMenu.addSeparator();
+    fileMenu.add(createMenuItem(MainController.CMD_EXIT));
 
+    BasicMenuItem aboutItem = new BasicMenuItem();
+    aboutItem.setAction(getController().getAction(MainController.CMD_SHOW_ABOUT_DIALOG));
+    helpMenu.add(aboutItem);
+  }
 
-   public void addTab(TabbedView view) {
-      tabbedPane.add(view.getTabName(), view.getPane());
-   } // addTab
+  public void addTab(TabbedView view) {
+    tabbedPane.add(view.getTabName(), view.getPane());
+  } // addTab
 
+  public void removeAllTabs() {
+    tabbedPane.removeAll();
+  } // removeAllTabs
 
-   public void removeAllTabs() {
-      tabbedPane.removeAll();
-   } // removeAllTabs
-
-
-   private BasicMenuItem createMenuItem(String action) {
-      BasicMenuItem item = new BasicMenuItem();
-      item.setAction(getController().getAction(action));
-      return item;
-   }
+  private BasicMenuItem createMenuItem(String action) {
+    BasicMenuItem item = new BasicMenuItem();
+    item.setAction(getController().getAction(action));
+    return item;
+  }
 
 }
