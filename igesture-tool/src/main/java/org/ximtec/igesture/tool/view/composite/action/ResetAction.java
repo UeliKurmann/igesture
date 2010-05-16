fe97cdf41b91bf12263d0f51e9bd8a5ec7f36f1f
bpuype <bpuype@f93bbda9-e428-0410-8f24-ec21284828af>
2010-05-16 13:16:10 +0000

    Added tab to manually test the composite gestures

---
+++ igesture-tool/src/main/java/org/ximtec/igesture/tool/view/composite/action/ResetAction.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.ximtec.igesture.tool.view.composite.action;
+
+import java.awt.event.ActionEvent;
+
+import org.ximtec.igesture.tool.GestureConstants;
+import org.ximtec.igesture.tool.core.Command;
+import org.ximtec.igesture.tool.core.Controller;
+import org.ximtec.igesture.tool.core.LocateableAction;
+import org.ximtec.igesture.tool.view.composite.CompositeController;
+
+/**
+ * @author Björn Puype, bpuype@gmail.com
+ *
+ */
+public class ResetAction extends LocateableAction {
+
+	private Controller controller;
+	
+	public ResetAction(Controller controller)
+	{
+		super(GestureConstants.COMPOSITE_RESET_ACTION,controller.getLocator());
+		this.controller = controller;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Command command = new Command(CompositeController.CMD_RESET);
+		controller.execute(command);
+	}
+
+}
