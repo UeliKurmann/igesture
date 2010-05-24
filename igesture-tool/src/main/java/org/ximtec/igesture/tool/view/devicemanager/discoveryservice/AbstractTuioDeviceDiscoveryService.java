38ca78f086db7a1c644fbe4d711a4a1b7887b94d
bsigner <bsigner@f93bbda9-e428-0410-8f24-ec21284828af>
2010-05-24 15:34:56 +0000

    - Cleanup

--- igesture-tool/src/main/java/org/ximtec/igesture/tool/view/devicemanager/discoveryservice/AbstractTuioDeviceDiscoveryService.java
+++ igesture-tool/src/main/java/org/ximtec/igesture/tool/view/devicemanager/discoveryservice/AbstractTuioDeviceDiscoveryService.java
@@ -27,12 +27,13 @@ import org.ximtec.igesture.util.XMLParser;
 
 /**
  * @author Björn Puype, bpuype@gmail.com
- *
+ * 
  */
 public abstract class AbstractTuioDeviceDiscoveryService implements
 		DeviceDiscoveryService {
 
-	private static final Logger LOGGER = Logger.getLogger(AbstractTuioDeviceDiscoveryService.class.getName());
+	private static final Logger LOGGER = Logger
+			.getLogger(AbstractTuioDeviceDiscoveryService.class.getName());
 	/**
 	 * First port of range to scan
 	 */
@@ -44,111 +45,128 @@ public abstract class AbstractTuioDeviceDiscoveryService implements
 	/**
 	 * created devices
 	 */
-	private Set<AbstractGestureDevice<?,?>> devices;
+	private Set<AbstractGestureDevice<?, ?>> devices;
 	/**
 	 * Mapping between port and name
 	 */
 	private Map<Integer, String> map;
-	
-	
+
 	private String typeToDiscover;
 	private Class<?> clazzToInstantiate;
-	
+
 	/**
 	 * Constructor
 	 */
-	public AbstractTuioDeviceDiscoveryService(String type, Class<?> clazz)
-	{
+	public AbstractTuioDeviceDiscoveryService(String type, Class<?> clazz) {
 		typeToDiscover = type;
 		clazzToInstantiate = clazz;
-		
-		//get port range to scan from the properties file
+
+		// get port range to scan from the properties file
 		Properties properties = new Properties();
 		try {
-			properties.loadFromXML(new FileInputStream(GestureConstants.PROPERTIES));
-			startPort = Integer.parseInt(properties.getProperty(Property.TUIO_START_PORT));
-			endPort = Integer.parseInt(properties.getProperty(Property.TUIO_END_PORT));
+			properties.loadFromXML(new FileInputStream(
+					GestureConstants.PROPERTIES));
+			startPort = Integer.parseInt(properties
+					.getProperty(Property.TUIO_START_PORT));
+			endPort = Integer.parseInt(properties
+					.getProperty(Property.TUIO_END_PORT));
 		} catch (Exception e) {
-			LOGGER.log(Level.WARNING,"Could not load the tuio port range property from file. Falling back on the default port: 3333.",e);
+			LOGGER
+					.log(
+							Level.WARNING,
+							"Could not load the TUIO port range property. Using the default port '3333'.",
+							e);
 			startPort = TuioConstants.DEFAULT_PORT;
 			endPort = startPort;
 		}
-		
+
 		map = new HashMap<Integer, String>();
-		
-		devices = new HashSet<AbstractGestureDevice<?,?>>();
-		
-		//get the registered tuio services
-		XMLParser parser = new XMLParser(){
+		devices = new HashSet<AbstractGestureDevice<?, ?>>();
+
+		// get the registered tuio services
+		XMLParser parser = new XMLParser() {
 
 			@Override
-			public void execute(ArrayList<NodeList> nodeLists) 
-			{
+			public void execute(ArrayList<NodeList> nodeLists) {
 				try {
 					// read the port number
-					int port = Integer.parseInt(((Node)nodeLists.get(0).item(0)).getNodeValue());
+					int port = Integer.parseInt(((Node) nodeLists.get(0)
+							.item(0)).getNodeValue());
 					// read the service name
-					String deviceName = ((Node)nodeLists.get(1).item(0)).getNodeValue();
+					String deviceName = ((Node) nodeLists.get(1).item(0))
+							.getNodeValue();
 					// read in the types
-					String type = ((Node)nodeLists.get(2).item(0)).getNodeValue();
+					String type = ((Node) nodeLists.get(2).item(0))
+							.getNodeValue();
 					String[] types = type.split(",");
-					for(String s : types)
-					{
-						if(typeToDiscover.equals(s))
-						{
+					for (String s : types) {
+						if (typeToDiscover.equals(s)) {
 							// put it in the map
-							map.put(port,deviceName);
+							map.put(port, deviceName);
 							break;
 						}
-							
-					}					
+
+					}
 				} catch (NumberFormatException e) {
-					LOGGER.log(Level.WARNING,"Invalid Port Number. The corresponding Tuio device type was not added.",e);
+					LOGGER
+							.log(
+									Level.WARNING,
+									"Invalid Port Number. The corresponding Tuio device type was not added.",
+									e);
 				} catch (NullPointerException e) {
-					LOGGER.log(Level.WARNING, "An empty node was encountered. The corresponding Tuio device type was not added.",e);
+					LOGGER
+							.log(
+									Level.WARNING,
+									"An empty node was encountered. The corresponding Tuio device type was not added.",
+									e);
 				}
 			}
-			
+
 		};
 		ArrayList<String> textNodes = new ArrayList<String>();
 		textNodes.add("port");
 		textNodes.add("name");
 		textNodes.add("type");
 		try {
-			parser.parse(System.getProperty("user.dir")+System.getProperty("file.separator")+GestureConstants.XML_TUIO, "device", textNodes);
+			parser.parse(System.getProperty("user.dir")
+					+ System.getProperty("file.separator")
+					+ GestureConstants.XML_TUIO, "device", textNodes);
 		} catch (IOException e) {
-			LOGGER.log(Level.SEVERE,"Could not find "+GestureConstants.XML_TUIO,e);//TODO
+			LOGGER.log(Level.SEVERE, "Could not find "
+					+ GestureConstants.XML_TUIO, e);// TODO
 		} catch (Exception e) {
-			LOGGER.log(Level.SEVERE,"Could not parse "+GestureConstants.XML_TUIO,e);//TODO
+			LOGGER.log(Level.SEVERE, "Could not parse "
+					+ GestureConstants.XML_TUIO, e);// TODO
 		}
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.ximtec.igesture.tool.view.devicemanager.discoveryservice.DeviceDiscoveryService#discover()
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.ximtec.igesture.tool.view.devicemanager.discoveryservice.
+	 * DeviceDiscoveryService#discover()
 	 */
 	@Override
 	public Set<AbstractGestureDevice<?, ?>> discover() {
-		
-		LOGGER.log(Level.INFO,"Device discovery started!");
-		
-		//for each port in the defined range
-		for(int port = startPort; port <= endPort; port++)
-		{
-			if(map.containsKey(port))
-			{
-				
+		LOGGER.log(Level.INFO, "Device discovery started!");
+
+		// for each port in the defined range
+		for (int port = startPort; port <= endPort; port++) {
+			if (map.containsKey(port)) {
+
 				try {
-					Constructor ctor = clazzToInstantiate.getConstructor(Integer.class);
-					AbstractGestureDevice<?,?> device = (AbstractGestureDevice<?, ?>) ctor.newInstance(new Integer(port));
+					Constructor ctor = clazzToInstantiate
+							.getConstructor(Integer.class);
+					AbstractGestureDevice<?, ?> device = (AbstractGestureDevice<?, ?>) ctor
+							.newInstance(new Integer(port));
 					String name = map.get(port);
-					if(name != null && !name.isEmpty())
-					{
+					if (name != null && !name.isEmpty()) {
 						device.setName(name);
-					}
-					else
+					} else
 						name = "Tuio Service";
-					LOGGER.log(Level.INFO,"Device discovered: "+name+" on port "+port);
-					devices.add(device);					
+					LOGGER.log(Level.INFO, "Device discovered: " + name
+							+ " on port " + port);
+					devices.add(device);
 				} catch (SecurityException e) {
 					e.printStackTrace();
 				} catch (NoSuchMethodException e) {
@@ -164,17 +182,20 @@ public abstract class AbstractTuioDeviceDiscoveryService implements
 				}
 			}
 		}
-		
-		LOGGER.log(Level.INFO,"Device discovery completed!");
-		
-		if(devices.isEmpty())
-			LOGGER.log(Level.INFO,"No devices discovered.");
-		
+
+		LOGGER.log(Level.INFO, "Device discovery completed!");
+
+		if (devices.isEmpty())
+			LOGGER.log(Level.INFO, "No devices discovered.");
+
 		return devices;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.ximtec.igesture.tool.view.devicemanager.discoveryservice.DeviceDiscoveryService#dispose()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.ximtec.igesture.tool.view.devicemanager.discoveryservice.
+	 * DeviceDiscoveryService#dispose()
 	 */
 	@Override
 	public void dispose() {
