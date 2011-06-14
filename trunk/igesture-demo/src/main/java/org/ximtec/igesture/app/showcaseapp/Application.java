bea1882e0b29549dab81dc89a20edb6d04237d1f
kurmannu <kurmannu@f93bbda9-e428-0410-8f24-ec21284828af>
2011-06-14 21:28:14 +0000

    Fixes in the demo application.

--- trunk/igesture-demo/src/main/java/org/ximtec/igesture/app/showcaseapp/Application.java
+++ trunk/igesture-demo/src/main/java/org/ximtec/igesture/app/showcaseapp/Application.java
@@ -23,7 +23,6 @@
  * 
  */
 
-
 package org.ximtec.igesture.app.showcaseapp;
 
 import java.awt.Color;
@@ -38,7 +37,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import org.sigtec.ink.Note;
-import org.sigtec.ink.Point;
 import org.sigtec.util.Constant;
 import org.ximtec.igesture.Recogniser;
 import org.ximtec.igesture.algorithm.AlgorithmException;
@@ -54,12 +52,11 @@ import org.ximtec.igesture.configuration.Configuration;
 import org.ximtec.igesture.core.DigitalDescriptor;
 import org.ximtec.igesture.core.Gesture;
 import org.ximtec.igesture.core.GestureSet;
+import org.ximtec.igesture.core.ResultSet;
 import org.ximtec.igesture.event.GestureActionManager;
-import org.ximtec.igesture.io.GestureDevice;
 import org.ximtec.igesture.io.GestureEventListener;
-import org.ximtec.igesture.io.mouseclient.MouseReader;
+import org.ximtec.igesture.io.mouseclient.SwingMouseReader;
 import org.ximtec.igesture.util.XMLTool;
-
 
 /**
  * @version 1.0 Nov 2006
@@ -68,113 +65,123 @@ import org.ximtec.igesture.util.XMLTool;
  */
 public class Application implements GestureEventListener {
 
-   private static final Logger LOGGER = Logger.getLogger(Application.class
-         .getName());
+	private static final Logger LOGGER = Logger.getLogger(Application.class
+			.getName());
 
-   private Recogniser recogniser;
+	private Recogniser recogniser;
 
-   private GestureDevice<Note, Point> client;
+	private SwingMouseReader client;
 
-   private JFrame frame;
+	private JFrame frame;
 
-   private BufferedImage bufferedImage;
+	private BufferedImage bufferedImage;
+
+	public Application() {
+		initialiseGUI();
+		initGestures();
+		initGestureInputFrame();
+	}
+
+	private void initGestureInputFrame() {
+		JFrame frame = new JFrame();
+		frame.setTitle("Input Frame - Draw your Gesture");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(440, 620);
+		frame.add(client.getPanel());
+		frame.setLocation(440, 0);
+		frame.setVisible(true);
+	}
+
+	private void initGestures() {
+		Configuration configuration = XMLTool.importConfiguration(ClassLoader
+				.getSystemResourceAsStream("rubineconfiguration.xml"));
+		GestureSet gestureSet = XMLTool.importGestureSet(ClassLoader
+				.getSystemResourceAsStream("demogestures.xml"));
+		GestureActionManager eventManager = new GestureActionManager(){
+			@Override
+			public synchronized void handle(ResultSet resultSet) {
+				super.handle(resultSet);
+				client.clear();
+			}
+		};
+		eventManager.registerRejectEvent(new RejectEventHandler(){
+			
+		});
+		Style style = new Style();
+		StyleEventHandler styleEventHandler = new StyleEventHandler(style);
+		Graphics2D graphic = (Graphics2D) bufferedImage.getGraphics();
+		DrawEventHandler drawEventHandler = new DrawEventHandler(graphic, style);
+		gestureSet.getGestureClass("Rectangle").addDescriptor(
+				DigitalDescriptor.class, new RectangleDescriptor());
+		gestureSet.getGestureClass("LeftRight").addDescriptor(
+				DigitalDescriptor.class, new LineDescriptor());
+		gestureSet.getGestureClass("Triangle").addDescriptor(
+				DigitalDescriptor.class, new TriangleDescriptor());
+		gestureSet.getGestureClass("Arrow").addDescriptor(
+				DigitalDescriptor.class, new ArrowDescriptor());
+		eventManager.registerEventHandler("Rectangle", drawEventHandler);
+		eventManager.registerEventHandler("LeftRight", drawEventHandler);
+		eventManager.registerEventHandler("Triangle", drawEventHandler);
+		eventManager.registerEventHandler("Arrow", drawEventHandler);
+		eventManager.registerEventHandler("Delete", new DeleteEventHandler(
+				graphic));
+		eventManager.registerEventHandler("Red", styleEventHandler);
+		eventManager.registerEventHandler("Black", styleEventHandler);
+		eventManager.registerEventHandler("Yellow", styleEventHandler);
+		eventManager.registerEventHandler("Thin", styleEventHandler);
+		eventManager.registerEventHandler("Fat", styleEventHandler);
+		configuration.addGestureSet(gestureSet);
+
+		try {
+			recogniser = new Recogniser(configuration);
+			recogniser.addGestureHandler(eventManager);
+		} catch (AlgorithmException e) {
+			LOGGER.log(Level.SEVERE, Constant.EMPTY_STRING, e);
+		}
+
+		client = new SwingMouseReader();
+		client.init();
+		client.addGestureHandler(this);
+
+	} // initGestures
+
+	private void initialiseGUI() {
+		frame = new JFrame();
+		frame.setTitle("Output Frame");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(440, 620);
+		bufferedImage = new BufferedImage(420, 600, BufferedImage.TYPE_INT_ARGB);
+		bufferedImage.getGraphics().setColor(Color.WHITE);
+		bufferedImage.getGraphics().fillRect(0, 0, 420, 600);
+		bufferedImage.getGraphics().setColor(Color.BLACK);
+		JLabel label = new JLabel();
+		label.setSize(420, 620);
+		label.setIcon(new ImageIcon(bufferedImage));
+		frame.add(label);
+		frame.setVisible(true);
 
 
-   public Application() {
-      initialiseGUI();
-      initGestures();
-   }
+	} // initialiseGUI
 
+	public static void main(String[] args) {
+		new Application();
+	}
 
-   private void initGestures() {
-      Configuration configuration = XMLTool.importConfiguration(ClassLoader
-            .getSystemResourceAsStream("rubineconfiguration.xml"));
-      GestureSet gestureSet = XMLTool.importGestureSet(ClassLoader
-            .getSystemResourceAsStream("demogestures.xml"));
-      GestureActionManager eventManager = new GestureActionManager();
-      eventManager.registerRejectEvent(new RejectEventHandler());
-      Style style = new Style();
-      StyleEventHandler styleEventHandler = new StyleEventHandler(style);
-      Graphics2D graphic = (Graphics2D)bufferedImage.getGraphics();
-      DrawEventHandler drawEventHandler = new DrawEventHandler(graphic, style);
-      gestureSet.getGestureClass("Rectangle").addDescriptor(
-            DigitalDescriptor.class, new RectangleDescriptor());
-      gestureSet.getGestureClass("LeftRight").addDescriptor(
-            DigitalDescriptor.class, new LineDescriptor());
-      gestureSet.getGestureClass("Triangle").addDescriptor(
-            DigitalDescriptor.class, new TriangleDescriptor());
-      gestureSet.getGestureClass("Arrow").addDescriptor(DigitalDescriptor.class,
-            new ArrowDescriptor());
-      eventManager.registerEventHandler("Rectangle", drawEventHandler);
-      eventManager.registerEventHandler("LeftRight", drawEventHandler);
-      eventManager.registerEventHandler("Triangle", drawEventHandler);
-      eventManager.registerEventHandler("Arrow", drawEventHandler);
-      eventManager.registerEventHandler("Delete",
-            new DeleteEventHandler(graphic));
-      eventManager.registerEventHandler("Red", styleEventHandler);
-      eventManager.registerEventHandler("Black", styleEventHandler);
-      eventManager.registerEventHandler("Yellow", styleEventHandler);
-      eventManager.registerEventHandler("Thin", styleEventHandler);
-      eventManager.registerEventHandler("Fat", styleEventHandler);
-      configuration.addGestureSet(gestureSet);
-      
+	@Override
+	public void handleChunks(List<?> chunks) {
+		// TODO Auto-generated method stub
 
-      try {
-         recogniser = new Recogniser(configuration);
-         recogniser.addGestureHandler(eventManager);
-      }
-      catch (AlgorithmException e) {
-         LOGGER.log(Level.SEVERE, Constant.EMPTY_STRING, e);
-      }
+	}
 
-      client = new MouseReader();
-      client.init();
-      client.addGestureHandler(this);
+	@Override
+	public void handleGesture(Gesture<?> gesture) {
 
-   } // initGestures
-
-
-   private void initialiseGUI() {
-      frame = new JFrame();
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setSize(440, 620);
-      bufferedImage = new BufferedImage(420, 600, BufferedImage.TYPE_INT_ARGB);
-      bufferedImage.getGraphics().setColor(Color.WHITE);
-      bufferedImage.getGraphics().fillRect(0, 0, 420, 600);
-      bufferedImage.getGraphics().setColor(Color.BLACK);
-      JLabel label = new JLabel();
-      label.setSize(420, 620);
-      label.setIcon(new ImageIcon(bufferedImage));
-      frame.add(label);
-      frame.setVisible(true);
-   } // initialiseGUI
-
-
-
-
-   public static void main(String[] args) {
-      new Application();
-   }
-
-
-   @Override
-   public void handleChunks(List< ? > chunks) {
-      // TODO Auto-generated method stub
-
-   }
-
-
-   @Override
-   public void handleGesture(Gesture< ? > gesture) {
-
-      if (gesture.getGesture() instanceof Note) {
-         Note note = (Note)gesture.getGesture();
-         Note clone = (Note)note.clone();
-         clone.scale(2);
-
-         recogniser.recognise(clone);
-         frame.repaint();
-      }
-   }
+		if (gesture.getGesture() instanceof Note) {
+			Note note = (Note) gesture.getGesture();
+			Note clone = (Note) note.clone();
+			recogniser.recognise(clone);
+			frame.repaint();
+		}
+	}
 
 }
